3e6b34b48cc2d43f0e7fc7113c3f0ba45e3834ce
muhsin <muhsin@butomy.in>
2024-01-02 22:11:58 +0530

    gsap smooth scroll correction

--- src/script.js
+++ src/script.js
@@ -12,48 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-
-//   window.addEventListener("scroll", function () {
-//     var navbar = document.getElementById('navbar');
-    
-//     if (window.pageYOffset > 0) {
-//         navbar.style.position = "fixed";
-//         navbar.classList.add('fixedNavbar'); 
-//         navbar.classList.remove('staticNavbar');
-//     } else {
-//         navbar.style.position = "static";
-//         navbar.classList.remove('fixedNavbar'); 
-//         navbar.classList.add('staticNavbar'); 
-//     }
-// });
-
-const tl = gsap.timeline();
-gsap.set(tl, { opacity: 0, });
-tl.from(".line span", 1.8, {
-  y:200,
-  ease:"power4.out",
-  delay: 0.5,
-  skewY: 7,
-  stagger: {
-    amount:0.1
-  }
-})
-// smooth scroll 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-ScrollTrigger.normalizeScroll(true)
-
-let smoother = ScrollSmoother.create({
-  smooth: 2,
-  effects: true,
-  normalizeScroll: true
-});
-
-ScrollTrigger.create({
-  start: 'top -80',
-  end: 99999,
-  toggleClass: {className: 'main-tool-bar--scrolled', targets: '.main-tool-bar'}
-});
-
+  
 // json file 
 document.addEventListener("DOMContentLoaded", function () {
   var animation = bodymovin.loadAnimation({
@@ -89,3 +48,37 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   });
 });
+
+
+const tl = gsap.timeline();
+gsap.set(tl, { opacity: 0, });
+tl.from(".line span", 1.8, {
+  y:200,
+  ease:"power4.out",
+  delay: 0.5,
+  skewY: 7,
+  stagger: {
+    amount:0.1
+  }
+})
+// smooth scroll 
+gsap.registerPlugin(ScrollTrigger);
+ ScrollTrigger.normalizeScroll(true)
+
+ScrollTrigger.create({
+  start: 'top -80',
+  end: 99999,
+  toggleClass: {className: 'main-tool-bar--scrolled', targets: '.main-tool-bar'}
+});
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
